c544a20b8cd98035b8c2b2fb005fc3fd0a3dfd6b
David Hodo <david.hodo@is4s.com>
2017-08-04 22:22:44 -0500

    switch to r15b

--- build-android.sh
+++ build-android.sh
@@ -263,6 +263,11 @@ case "$NDK_RN" in
 		CXXPATH=$ANDROID_STANDALONE_TOOLCHAIN/bin/clang++
 		TOOLSET=clang-androidR14b
 		;;
+	"15b")
+		TOOLCHAIN=${TOOLCHAIN:-llvm}
+		CXXPATH=$ANDROID_STANDALONE_TOOLCHAIN/bin/clang++
+		TOOLSET=clang-androidR15b
+		;;
 	*)
 		echo "Undefined or not supported Android NDK version!"
 		exit 1
